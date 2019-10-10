fdd682020500d707e2fc930f334a485cb680b38c
Paul McMillan <paul@astro.lu.se>
2019-10-10 16:45:04 +0200

    Do names better. Bold text for my name.

--- PublicationListMaker.py
+++ PublicationListMaker.py
@@ -6,10 +6,34 @@ import ads
 import subprocess
 
 
-
-
 FullName = 'Paul McMillan'
 Surname = 'McMillan'
+filename = Surname + '_PublicationList.tex'
+
+
+def AuthorNameAbbreviation(author) :
+    # Convert to Latex
+    fullname = unicode_to_latex(author)
+    commaplace = fullname.find(',')
+    if commaplace >=0 :
+        if(fullname[:commaplace] == Surname) :
+            return '\\textbf{'+ fullname[:commaplace+3] +'.}'
+        else :
+            return fullname[:commaplace+3] + '.'
+    else :
+        return fullname
+        
+
+def JournalVolumePage(article) :
+    if unicode_to_latex(article.bibstem[0]) == 'arXiv' :
+        return article.page[0]
+    elif article.volume is None :
+        return  unicode_to_latex(article.bibstem[0])+', in press'
+    else :
+        return unicode_to_latex(article.bibstem[0])+', ' + str(article.volume) \
+            +', ' + article.page[0]
+
+
 
 # My ORCID. Replace with your own if you so desire
 ORCID = '0000-0002-8861-2620'
@@ -52,7 +76,6 @@ Header += "\n\n\n\\begin{document}\n\n\\begin{enumerate}\n"
 # end text
 Footer = "\\end{enumerate}\n\n\\end{document}\n"
 
-filename = Surname + '_PublicationList.tex'
 
 fileout = open(filename,'w')
 
@@ -61,22 +84,28 @@ fileout.write(Header)
 for article in articles :
     # enumerated list
     fileout.write ('\\item ``' + unicode_to_latex(article.title[0])+'\'\', ')
+    # Solo papers
     if len(article.author) ==1 :
-        fileout.write ( unicode_to_latex(article.author_norm[0]) + '. ')
+        fileout.write ( AuthorNameAbbreviation(article.author[0]) + ' ')
+    # Short author lists
     elif len(article.author) <=4 :
         for i in range(len(article.author)-1) :
-            fileout.write ( unicode_to_latex(article.author_norm[i]) +'.')
+            fileout.write ( AuthorNameAbbreviation(article.author[i]) )
             if (i<len(article.author)-2): fileout.write(', ')
         fileout.write(' \\& ' +
-                      unicode_to_latex(article.author_norm[len(article.author)-1]) +'. ')
+                      AuthorNameAbbreviation(article.author[len(article.author)-1]) +' ')
     else :
+    # Long author lists
         for i in range(4) :
-            fileout.write(unicode_to_latex(article.author_norm[i]) +'., ')
+            fileout.write(AuthorNameAbbreviation(article.author[i]) +', ')
         fileout.write ('et al. ')
-    fileout.write ('('+ article.year + '), ' + \
-            unicode_to_latex(article.bibstem[0])+', ' + str(article.volume) \
-            +', ' + article.page[0] +'. (Citations to date ' + str(article.citation_count) +')\n\n')
- 
+
+    fileout.write ('('+ article.year + '), ' + JournalVolumePage(article) + '.')
+    # Citations?
+    if article.citation_count > 0 :
+        fileout.write (' (Citations to date ' + str(article.citation_count) +'.)\n\n')
+    else :
+        fileout.write ('\n\n')
 fileout.write(Footer)
 
 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CVmaker
+
+Requires python packages ads and pylatexenc for reading NASA ADS, and converting the unicode to latex
+
